97da6ce75619384c742825b3f4d8a155a05ffa73
LeonardoDiazO <maximun_neo@hotmail.com>
2024-08-25 23:54:01 -0500

    fead variables globales en nodeJS

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 console.log('Hola, mundo ')
+console.info('Hola, mundo');
+console.error('Hola, mundo');
+console.log(typeof windows); //OJO QUE ESTO DA UNIFINE
+console.log(globalThis); // Es una variable global en toda nuestra aplicación, siempre tengo acceso
+
